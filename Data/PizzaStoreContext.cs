04e9d2e306aa3ba862235f030079b2ee4ec1b6e5
sdiagne148 <sdiagne148@gmail.com>
2023-09-15 14:30:46 -0400

    Use pages, routing, and layouts to improve Blazor navigation

--- Data/PizzaStoreContext.cs
+++ Data/PizzaStoreContext.cs
@@ -12,6 +12,19 @@ namespace BlazingPizza.Data
         {
         }
 
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
         public DbSet<PizzaSpecial> Specials { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuring a many-to-many special -> topping relationship that is friendly for serialization
+            modelBuilder.Entity<PizzaTopping>().HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+            modelBuilder.Entity<PizzaTopping>().HasOne<Pizza>().WithMany(ps => ps.Toppings);
+            modelBuilder.Entity<PizzaTopping>().HasOne(pst => pst.Topping).WithMany();
+        }
     }    
 }
